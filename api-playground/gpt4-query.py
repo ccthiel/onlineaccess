1ee75a93b0162ed04d9bc2fe07bc6d9d72259b2d
Chris Thiel <chris.thiel@oracle.com>
2023-09-15 16:44:51 -0600

    playing

--- api-playground/gpt4-query.py
+++ api-playground/gpt4-query.py
@@ -1,4 +1,4 @@
-from llama_index import VectorStoreIndex, StorageContext, ServiceContext, set_global_service_context, load_index_from_storage
+from llama_index import ListIndex, StorageContext, ServiceContext, set_global_service_context, load_index_from_storage
 from llama_index.llms import OpenAI
 
 # define LLM
@@ -8,12 +8,13 @@ llm = OpenAI(model="gpt-4-0613", temperature=0)
 service_context = ServiceContext.from_defaults(llm=llm)
 set_global_service_context(service_context)
 
-# rebuild storage context
+methods = [method for method in vars(ListIndex) if callable(getattr(ListIndex, method))]
+print(methods)
+
 storage_context = StorageContext.from_defaults(persist_dir=".")
 
-# load index
 index = load_index_from_storage(storage_context)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("List all the poisons")
-print(response)
+response = query_engine.query("give me a list of all poisons")
+print(type(response))
--- api-playground/gpt4-store.py
+++ api-playground/gpt4-store.py
@@ -1,6 +1,5 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
-
-from llama_index import VectorStoreIndex, ServiceContext, set_global_service_context
+from pathlib import Path
+from llama_index import ListIndex, SimpleDirectoryReader, ServiceContext, set_global_service_context, download_loader
 from llama_index.llms import OpenAI
 
 # define LLM
@@ -10,9 +9,12 @@ llm = OpenAI(model="gpt-4-0613", temperature=0)
 service_context = ServiceContext.from_defaults(llm=llm)
 set_global_service_context(service_context)
 
+# JSON Reader
+JSONReader = download_loader("JSONReader")
 
-documents = SimpleDirectoryReader(input_dir='./test', recursive=True).load_data()
+loader = JSONReader()
+documents = loader.load_data(Path("./test/poisons.json"))
 print(f"loaded {len(documents)} documents for storing")
 
-index = VectorStoreIndex.from_documents(documents)
+index = ListIndex.from_documents(documents)
 index.storage_context.persist(persist_dir=".")
