bff26f794b291db0561bf40f80e2f61b20d03f65
Chris Thiel <chris.thiel@oracle.com>
2024-01-15 14:40:46 -0700

    More moving aroubnd

--- api-playground/simple.py
+++
@@ -1,7 +0,0 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader('./test').load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("List all the poisons")
-print(response)
